7b14fdcc53774caec0744336c706c87de92ef9b1
Marcos <marcosmaiafidelis777@outlook.com>
2022-04-14 20:05:59 -0300

    validação

--- script/Login/login.js
+++ script/Login/login.js
@@ -6,9 +6,18 @@ button.addEventListener('click', button => {
 
     if (email.value == ""){
         alert("Digite seu email")
+    } else{
+        
     }
     if (password.value == ""){
         alert("Digite sua senha")
+    }  if (email.value !== "@") {
+        alert("falta o @")
+    } 
+    if (email.value !== ".") {
+        alert("falta o ponto")
     }
-}) 
-  
+    if (password.value < 6) {
+        alert("minimo 6 caracter")
+    }
+})
