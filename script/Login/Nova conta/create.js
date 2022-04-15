6bdebfd1bdbe5ea0fe3606eb0f6ebf3929e20f5e
Marcos <marcosmaiafidelis777@outlook.com>
2022-04-15 11:05:04 -0300

    validação da nova conta

--- script/Login/Nova conta/create.js
+++ script/Login/Nova conta/create.js
@@ -3,8 +3,16 @@ const button = document.getElementById('continue')
 button.addEventListener('click', (event) => {
     event.preventDefault()
     let nome = document.getElementById('name')
+    let email = document.getElementById('email')
+    let password = document.getElementById('password')
 
     if(nome.value == "") {
         nome.classList.add("errorInput")
     }
-})
+    if(email.value == "" || email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1){
+        email.classList.add("errorInput")
+    }
+    if(password.value.length <= 5) {
+        password.classList.add("errorInput")
+    }
+})  
