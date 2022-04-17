435e788588415a28b8381883b5aa92d4a457fd31
Marcos <marcosmaiafidelis777@outlook.com>
2022-04-17 20:24:48 -0300

    Localização

--- script/Endereço/address.js
+++ script/Endereço/address.js
@@ -1,9 +1,16 @@
 'use strict'
 
 const cep = document.getElementById('cep')
+const showData = (result) => {
+    for(const campo in result){
+        if(document.querySelector('#'+campo)){
+            document.querySelector('#'+campo).value = result[campo]
+        }
+    }
+}
 
 cep.addEventListener('blur' ,(e)=>{
-    let search = cep.value
+    let search = cep.value.replace("-" , "")
     const options = {
         method: 'GET',
         mode: 'cors',
@@ -12,7 +19,7 @@ cep.addEventListener('blur' ,(e)=>{
 
     fetch(`https://viacep.com.br/ws/${search}/json/`, options)
     .then(response=>{response.json()
-        .then(data => console.log(data))
+        .then(data => showData(data))
     })
     .catch(e => console.log('Deu erro' + e,message))
     
