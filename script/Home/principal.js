76e4a167a5b8886a664f9a783be6df8419c0eadd
Marcos <marcosmaiafidelis777@outlook.com>
2022-04-18 21:24:33 -0300

    modal

--- script/Home/principal.js
+++ script/Home/principal.js
@@ -1,2 +1,17 @@
+function modal () {
+    let button = document.getElementById('modal')
+        .addEventListener('clikc', button => {
+            alert("oi")
+        })
+}
 
+
+
+
+function backHome () {
+    window.scroll({
+        behavior: "smooth",
+        top: 0
+    })
+}
 
