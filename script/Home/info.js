4d8195a92e719c504dd220e6cf3f3c992425608a
Marcos <marcosmaiafidelis777@outlook.com>
2022-04-21 13:43:10 -0300

    main

--- script/Home/info.js
+++ script/Home/info.js
@@ -1,9 +1,17 @@
 let pizzaJson = [
-    {id:1, name:'Tenis Lekes', img:'imagens/tenislekes.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
-];
+    {id:1, name: 'Bola adidas', img: 'imagens/Home/Slide secundário/bolaAdidas.png', price: 130 , description: 'Bola de futebol Adidas'},
+    {id:2, name: 'Corda', img: 'imagens/Home/Slide secundário/corda.png', price: 39 , description: 'Corda'},
+    {id:3, name: 'Creatina', img: 'imagens/Home/Slide secundário/creatinaIntegral.png', price: 55 , description: 'Creatina'},
+    {id:4, name: 'Básica feminina', img: 'imagens/Home/Slide secundário/básicaFeminina.png', price: 132.39 , description: 'Camisa básica feminina'},
+    {id:5, name: 'Esteira', img: 'imagens/Home/Slide secundário/esteira.png', price: 530.50 , description: 'Esteira'},
+    {id:6, name: 'Whey', img: 'imagens/Home/Slide secundário/max.png', price: 120 , description: 'Whey'},
+    {id:7, name: 'Pasta de amendoim', img: 'imagens/Home/Slide secundário/pastaAmendoim.png', price: 32.99 , description: 'Pasta de amendoim'},
+    {id:8, name: 'Camisa da seleção brasileira', img: 'imagens/Home/Slide secundário/seleçãoBrasileira.png', price: 259 , description: 'Camisa da seleção brasileira'},
+    {id:9, name: 'Short Adidas', img: 'imagens/Home/Slide secundário/shortAdidas.png', price: 89.99 , description: 'Short Adidas'},
+    {id:10, name: 'Short Adidas', img: 'imagens/Home/Slide secundário/shortAdidas2.png', price: 99.99 , description: 'Short Adidas'},
+    {id:11, name: 'Supino', img: 'imagens/Home/Slide secundário/supino.png', price: 800 , description: 'Supino'},
+    {id:12, name: 'Tênis feminino Nike', img: 'imagens/Home/Slide secundário/tenisFeminino.png', price: 249.99 , description: 'Tênis feminino Nike'},
+    {id:13, name: 'Short térmico Nike', img: 'imagens/Home/Slide secundário/termicaNike.png', price: 90 , description: 'Short Térmico Nike'},
+    {id:14, name: 'Vitamina B12', img: 'imagens/Home/Slide secundário/vitaminaB12.png', price: 55 , description: 'Vitamina B12'},
+    {id:15, name: 'YoPro', img: 'imagens/Home/Slide secundário/YoPro.png', price: 5.99 , description: 'YoPro Chocolate'}
+]
