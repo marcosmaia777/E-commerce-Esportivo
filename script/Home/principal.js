fee947c2c0354c03ba80ef75b84b185061e03907
Marcos <marcosmaiafidelis777@outlook.com>
2022-04-22 20:40:19 -0300

    main

--- script/Home/principal.js
+++ script/Home/principal.js
@@ -1,13 +1,34 @@
+
 imgJson.map((item , index)=> {
     let imgJson = document.querySelector('.models .item').cloneNode(true);
-    
+    //Informações dos produtos.
+    imgJson.setAttribute('data-key' , index)
     imgJson.querySelector('.item--name').innerHTML = item.name
     imgJson.querySelector('.item--desc').innerHTML = item.description
     imgJson.querySelector('.item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
     imgJson.querySelector('.item--img  img').src = item.img
+    //Modal.
+    imgJson.querySelector('a').addEventListener('click' , (e) => {
+        e.preventDefault()
+        let key = e.target.closest('.item').getAttribute('data-key');
+        
+       
+
+        
+
+        document.querySelector('.modal').style.opacity = 0
+        document.querySelector('.modal').style.display = 'flex';
+        setTimeout(() => {
+            document.querySelector('.modal').style.opacity = 1
+        }, 200)
+    });
+
 
     document.querySelector('.area').append( imgJson )
 })
+
+
+
 
 
 
@@ -21,5 +42,3 @@ function backHome () {
         top: 0
     })
 }
-
-
