790013e49aaeb29269aaed42bd9440fe3d7270e6
Marcos <marcosmaiafidelis777@outlook.com>
2022-04-23 00:19:04 -0300

    main

--- script/Home/principal.js
+++ script/Home/principal.js
@@ -1,11 +1,12 @@
 imgJson.map((item, index)=> {
+    //Preenchimento 
     let Item = document.querySelector('.models .item').cloneNode(true);
     Item.setAttribute('data-key' , index)
     Item.querySelector('.item--name').innerHTML = item.name
     Item.querySelector('.item--desc').innerHTML = item.description
     Item.querySelector('.item--img img').src = item.img
     Item.querySelector('.item--price').innerHTML = `${item.price.toFixed(2)}`
-
+    //Modal
     Item.querySelector('a').addEventListener('click' , (e) => {
         e.preventDefault()
         let key = e.target.closest('.item').getAttribute('data-key');
@@ -13,6 +14,12 @@ imgJson.map((item, index)=> {
         document.querySelector('.modal--desc').innerHTML = imgJson[key].description
         document.querySelector('.modal--big img').src = imgJson[key].img
         document.querySelector('.modal--actualPrice').innerHTML = `R$ ${imgJson[key].price.toFixed(2)}`
+
+        if (item.name === 'Short térmico Nike' || item.name === 'Tênis feminino Nike' || item.name === 'Short Adidas' || item.name === 'Camisa da seleção brasileira' || item.name === 'Básica feminina' ) {
+            document.querySelector('.modal--sizes').style.display = 'inline-flex'
+        } else {
+            document.querySelector('.modal--sizes').style.display = 'none'
+        }
 
         
 
