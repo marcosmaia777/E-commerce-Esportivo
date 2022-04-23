522f55bc4e29fda69aac1c1622ad19000cd8a7d5
Marcos <marcosmaiafidelis777@outlook.com>
2022-04-23 12:11:03 -0300

    modal

--- script/Home/principal.js
+++ script/Home/principal.js
@@ -6,7 +6,7 @@ imgJson.map((item, index)=> {
     Item.querySelector('.item--desc').innerHTML = item.description
     Item.querySelector('.item--img img').src = item.img
     Item.querySelector('.item--price').innerHTML = `${item.price.toFixed(2)}`
-    //Modal
+    //Abrir modal
     Item.querySelector('a').addEventListener('click' , (e) => {
         e.preventDefault()
         let key = e.target.closest('.item').getAttribute('data-key');
@@ -14,33 +14,26 @@ imgJson.map((item, index)=> {
         document.querySelector('.modal--desc').innerHTML = imgJson[key].description
         document.querySelector('.modal--big img').src = imgJson[key].img
         document.querySelector('.modal--actualPrice').innerHTML = `R$ ${imgJson[key].price.toFixed(2)}`
-
-        if (item.name === 'Short térmico Nike' || item.name === 'Tênis feminino Nike' || item.name === 'Short Adidas' || item.name === 'Camisa da seleção brasileira' || item.name === 'Básica feminina' ) {
-            document.querySelector('.modal--sizes').style.display = 'inline-flex'
-        } else {
+        //Botões de tamanho
+        if (item.id2 === 'Variados') {
+            document.querySelector('.modal--PMG').style.display = 'none'
             document.querySelector('.modal--sizes').style.display = 'none'
+        }if (item.id2 === 'Moda') {
+            document.querySelector('.modal--sizes').style.display = 'none'
+        }if (item.id2 === 'Calçado') {
+            document.querySelector('.modal--PMG').style.display = 'none'
         }
 
-        
-
-        document.querySelector('.modal').style.opacity = 0
-        document.querySelector('.modal').style.display = 'flex';
-        setTimeout(() => {
+    document.querySelector('.modal').style.opacity = 0
+    document.querySelector('.modal').style.display = 'flex';
+    setTimeout(() => {
             document.querySelector('.modal').style.opacity = 1
         }, 200)
     });
-
-
-
     document.querySelector('.area').append( Item )
 });
 
-
-
-
-
-
-
+//Eventos do modal 
 
 
 // scroll backHome
@@ -50,3 +43,13 @@ function backHome () {
         top: 0
     })
 }
+
+
+
+   
+
+
+
+
+
+
