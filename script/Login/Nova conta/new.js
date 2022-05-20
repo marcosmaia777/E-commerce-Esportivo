429b19212b14b2aaee5a8190a4355f5155a2b6a4
Marcos <marcosmaiafidelis777@outlook.com>
2022-05-20 17:11:58 -0300

    senha

--- script/Login/Nova conta/new.js
+++ script/Login/Nova conta/new.js
@@ -6,9 +6,36 @@ function save () {
 
     if(nome.value == "" &&
     email.value == "" || email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1 &&
-    password.value.length <= 5 &&
-    confir.value === password.value) {
-       alert('errado')
+    password.value.length <= 5) {
+        console.log('errado')
+        
+    } else {
+        vamo ()
+    }
+
+}
+ 
+function vamo () {
+    let dados = JSON.parse(localStorage.getItem('form') || '[]')
+dados.push(
+    {
+        nome: nome.value,
+        email: email.value,
+        password: password.value,
+        confirmation: confirmation.value
+    }
+)
+localStorage.setItem('form' , JSON.stringify(dados))
+}function save () {
+    let nome = document.getElementById('nome')
+    let email = document.getElementById('email')
+    let password = document.getElementById('password')
+
+    if(nome.value == "" &&
+    email.value == "" || email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1 &&
+    password.value.length >= 5) {
+        console.log('errado')
+        
     } else {
         vamo ()
     }
