be3f8b38a3ba771eee1222677aa15a81ed27a17f
Marcos <marcosmaiafidelis777@outlook.com>
2022-05-23 14:24:01 -0300

    ajustes

--- script/Login/Nova conta/new.js
+++ script/Login/Nova conta/new.js
@@ -1,21 +1,23 @@
-function save () {
+function cadastrar () {
     let nome = document.getElementById('nome')
     let email = document.getElementById('email')
     let password = document.getElementById('password')
     let confirmation = document.getElementById('confirmation')
 
-    if(nome.value == "" &&
-    email.value == "" || email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1 &&
-    password.value.length <= 5 && confirmation.value !== password.value) {
+    if(nome.value == "" ||  
+        email.value == "" || 
+        email.value.indexOf("@") == -1 || 
+        email.value.indexOf(".") == -1 || 
+        password.value.length <= 5 ||
+        confirmation.value !== password.value  ) {
         console.log('errado')
         
     } else {
-        cadastrar ()
+        save()
     }
-
 }
  
-function cadastrar () {
+function save () {
     let dados = JSON.parse(localStorage.getItem('form') || '[]')
 dados.push(
     {
