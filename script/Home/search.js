41375d34a8f2666f5c3af294cbcd3eb7efd2b810
Marcos <marcosmaiafidelis777@outlook.com>
2022-07-26 16:26:48 -0300

    search

--- script/Home/search.js
+++ script/Home/search.js
@@ -1,7 +1,19 @@
 document.addEventListener('keypress', function(e){
 
-    if(e.key === 'Enter') {
+    if(e.key === '' ) {
+        console.log('não tem nada')
+    }
+    else (e.key === 'Enter') 
         let button = document.getElementById('buscar');
         button.click();
     }
+);
+
+/*let button = document.getElementById('buscar')
+button.addEventListener('click', function(e){
+    e.preventDefault();
+    let input = document.getElementById('search')
+    let value = input.value
+    console.log(value) 
 })
+*/
